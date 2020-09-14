5124d25bcf85a64af434d8cb71e225b792c0a0f0
Xinger <u201717024@hust.edu.cn>
2020-09-14 15:50:31 +0800

    add index

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,23 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+let _data = require('../../data/data.js')
 Page({
   data: {
-
+    tasks: {},
+    loading: true,
+    slideButtons:[{
+      type: 'warn',
+      text: '删除',
+      src: '/page/weui/cell/icon_del.svg', // icon的路径
+    }]
   },
   onLoad: function () {
-    }
+    this.setData({tasks: _data.tasks, loading: false})
+    },
+  taskCheck: function(e){
+    console.log("check")
+  },
+  taskBegin: function(e){
+  }
 })
