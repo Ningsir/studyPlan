81191cb8e05ceba076d90948af659a5dbc6619b2
Xinger <u201717024@hust.edu.cn>
2020-09-16 15:54:15 +0800

    update api

--- config/api.js
+++ config/api.js
@@ -1,11 +1,14 @@
 const ApiRootURL = "https://www.ningxin.site/api"
 exports.default = {
-  createTask: ApiRootURL + "/task/createTask/",
-  getTask: ApiRootURL + "/task/getTask/",
-  submmit: ApiRootURL + "/task/submitRecord/",
-  getDailyData: ApiRootURL + "/data/getDailyData/",
-  monthAnalyze: ApiRootURL + "/data/monthAnalyze/",
-  timeDistribution: ApiRootURL + "/data/timeDistribution/",
-  createTag: ApiRootURL + "/admin/createCommonTag/",
-  deleteTag: ApiRootURL + "/admin/deleteCommonTag/"
+  createTask: ApiRootURL + "/task/createTask",
+  getTask: ApiRootURL + "/task/getTask",
+  submmit: ApiRootURL + "/task/submitRecord",
+  getDailyData: ApiRootURL + "/data/getDailyData",
+  monthAnalyze: ApiRootURL + "/data/monthAnalyze",
+  timeDistribution: ApiRootURL + "/data/timeDistribution",
+  createTag: ApiRootURL + "/admin/createCommonTag",
+  deleteTag: ApiRootURL + "/admin/deleteCommonTag",
+  adminLogin: ApiRootURL + "/auth/adminLogin",
+  getCurrentUser: ApiRootURL + "/auth/getCurrentUser",
+  login: ApiRootURL + "/auth/login"
 }
