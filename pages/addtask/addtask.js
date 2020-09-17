0efb929df05f57a1855b697b7a245df124c1caf8
Xinger <u201717024@hust.edu.cn>
2020-09-17 10:29:38 +0800

    获取后端数据

--- pages/addtask/addtask.js
+++ pages/addtask/addtask.js
@@ -1,5 +1,5 @@
 Page({
-  data:{
+  data: {
     task_name: "",
     types: ["单次任务", "多次任务"],
     task_type: "",
@@ -19,16 +19,30 @@ Page({
     maxHour: 20,
     radio: '1',
     newTag: '',
-    tags:["英语", "数学", "政治", "专业课", "不想学"],
+    tags: ["英语", "数学", "政治", "专业课", "不想学"],
     currenttag: [],
-    alertItems: [
-      {name: '是', value: '0', checked: true},
-      {name: '否', value: '1'}
+    alertItems: [{
+        name: '是',
+        value: '0',
+        checked: true
+      },
+      {
+        name: '否',
+        value: '1'
+      }
     ],
-    labels: [
-      {name: '英语', value: '0'},
-      {name: '政治', value: '1'},
-      {name: '数学', value: '2'}
+    labels: [{
+        name: '英语',
+        value: '0'
+      },
+      {
+        name: '政治',
+        value: '1'
+      },
+      {
+        name: '数学',
+        value: '2'
+      }
     ],
     dialogvisible: false,
     typeshow: false,
@@ -38,33 +52,35 @@ Page({
     endshow: false
   },
   //任务名称
-  onChange_taskname(event){
+  onChange_taskname(event) {
     this.setData({
       task_name: event.detail
     })
     console.log(this.data.task_name)
   },
   //任务标签对话框
-  dialogpopup(){
+  dialogpopup() {
     this.setData({
       dialogvisible: true
     })
   },
-  tagChange(event){
+  tagChange(event) {
     this.setData({
       newTag: event.detail
     })
   },
-  tag_button_click(event){
-    if(this.data.currenttag.indexOf(this.data.tags[event.currentTarget.id])<0){
+  tag_button_click(event) {
+    if (this.data.currenttag.indexOf(this.data.tags[event.currentTarget.id]) < 0) {
       this.data.currenttag.push(this.data.tags[event.currentTarget.id]);
-      this.setData({currenttag:this.data.currenttag}) //调用setData后才能重新渲染
+      this.setData({
+        currenttag: this.data.currenttag
+      }) //调用setData后才能重新渲染
     }
     console.log(this.data.currenttag)
   },
-  dialogconfirm(){ //点击添加标签按钮后触发的函数
-    if(this.data.newTag!=""){
-      if(this.data.currenttag.indexOf(this.data.newTag)<0){
+  dialogconfirm() { //点击添加标签按钮后触发的函数
+    if (this.data.newTag != "") {
+      if (this.data.currenttag.indexOf(this.data.newTag) < 0) {
         this.data.currenttag.push(this.data.newTag);
       }
     }
@@ -72,26 +88,42 @@ Page({
   },
   //任务类型弹出
   showPopup_tasktype() {
-    this.setData({ typeshow: true });
+    this.setData({
+      typeshow: true
+    });
   },
   onClose_tasktype() { //非取消按钮关闭
-    this.setData({ 
+    this.setData({
       typeshow: false,
       task_type: "",
       task_typeindex: 0
     });
   },
   onChange_type(event) {
-    const { picker, value, index } = event.detail;
-    this.setData({task_type: value, task_typeindex: index})
+    const {
+      picker,
+      value,
+      index
+    } = event.detail;
+    this.setData({
+      task_type: value,
+      task_typeindex: index
+    })
     console.log(this.data.task_type, this.data.task_typeindex)
   },
   onConfirm_type(event) {
-    const { picker, value, index } = event.detail;
-    this.setData({ typeshow: false });
+    const {
+      picker,
+      value,
+      index
+    } = event.detail;
+    this.setData({
+      typeshow: false
+    });
   },
-  onCancel_type() { //取消按钮
-    this.setData({ 
+  //取消按钮
+  onCancel_type() { 
+    this.setData({
       typeshow: false,
       task_type: "",
       task_typeindex: 0
@@ -99,26 +131,41 @@ Page({
   },
   //单次设置
   single_popup() {
-    this.setData({ singleshow: true });
+    this.setData({
+      singleshow: true
+    });
   },
   onClose_single() {
-    this.setData({ 
+    this.setData({
       singleshow: false,
       task_duration: "",
       task_durationindex: 0
     });
   },
   onChange_single(event) {
-    const { picker, value, index } = event.detail;
-    this.setData({task_duration: value, task_durationindex: index});
+    const {
+      picker,
+      value,
+      index
+    } = event.detail;
+    this.setData({
+      task_duration: value,
+      task_durationindex: index
+    });
     console.log(this.data.task_duration, this.data.task_durationindex)
   },
   onConfirm_single(event) {
-    const { picker, value, index } = event.detail;
-    this.setData({ singleshow: false });
+    const {
+      picker,
+      value,
+      index
+    } = event.detail;
+    this.setData({
+      singleshow: false
+    });
   },
   onCancel_single() {
-    this.setData({ 
+    this.setData({
       singleshow: false,
       task_duration: "",
       task_durationindex: 0
@@ -126,28 +173,40 @@ Page({
   },
   //多次频率设置
   frequency_popup() {
-    this.setData({ frequencyshow: true });
+    this.setData({
+      frequencyshow: true
+    });
   },
   onClose_frequency() {
-    this.setData({ 
+    this.setData({
       frequencyshow: false,
       task_frequency: "",
       task_frequencyindex: 0
     });
   },
   onChange_frequency(event) {
-    const { picker, value, index } = event.detail;
+    const {
+      picker,
+      value,
+      index
+    } = event.detail;
     this.setData({
       task_frequency: value,
       task_frequencyindex: index
     })
   },
   onConfirm_frequency(event) {
-    const { picker, value, index } = event.detail;
-    this.setData({ frequencyshow: false });
+    const {
+      picker,
+      value,
+      index
+    } = event.detail;
+    this.setData({
+      frequencyshow: false
+    });
   },
   onCancel_frequency() {
-    this.setData({ 
+    this.setData({
       frequencyshow: false,
       task_frequency: "",
       task_frequencyindex: 0
@@ -155,21 +214,29 @@ Page({
   },
   //开始、结束时间设置
   start_popup() {
-    this.setData({ startshow: true });
+    this.setData({
+      startshow: true
+    });
   },
   onClose_start() {
-    this.setData({ 
+    this.setData({
       startshow: false,
       currentstarttime: "12:01"
     });
   },
   onConfirm_start(event) {
-    const { picker, value, index } = event.detail;
+    const {
+      picker,
+      value,
+      index
+    } = event.detail;
     //提交
-    this.setData({ startshow: false });
+    this.setData({
+      startshow: false
+    });
   },
   onCancel_start() {
-    this.setData({ 
+    this.setData({
       startshow: false,
       currentstarttime: "12:01"
     });
@@ -182,23 +249,35 @@ Page({
   },
   //结束
   end_popup() {
-    this.setData({ endshow: true });
+    this.setData({
+      endshow: true
+    });
   },
   onClose_end() {
-    this.setData({ 
+    this.setData({
       endshow: false,
       currentendtime: '12:01'
     });
   },
   onChange_end(event) {
-    const { picker, value, index } = event.detail;
+    const {
+      picker,
+      value,
+      index
+    } = event.detail;
   },
   onConfirm_end(event) {
-    const { picker, value, index } = event.detail;
-    this.setData({ endshow: false });
+    const {
+      picker,
+      value,
+      index
+    } = event.detail;
+    this.setData({
+      endshow: false
+    });
   },
   onCancel_end() {
-    this.setData({ 
+    this.setData({
       endshow: false,
       currentendtime: '12:01'
     });
@@ -216,9 +295,19 @@ Page({
     });
   },
   onClick_radio(event) {
-    const { name } = event.currentTarget.dataset;
+    const {
+      name
+    } = event.currentTarget.dataset;
     this.setData({
       radio: name,
     });
+  },
+  save(){
+
+  },
+  cancel(){
+    wx.navigateBack({
+      delta: 1,
+    })
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,9 @@
 //index.js
 //获取应用实例
 const app = getApp()
+const api = require('../../config/api')
 let _data = require('../../data/data.js')
+const { default: toast } = require('../../miniprogram_npm/@vant/weapp/toast/toast')
 Page({
   data: {
     tasks: {},
@@ -16,8 +18,24 @@ Page({
     this.setData({tasks: _data.tasks, loading: false})
     },
   taskCheck: function(e){
-    console.log("check")
   },
   taskBegin: function(e){
+  },
+
+  getTasks(){
+    wx.request({
+      url: api.getTask, //仅为示例，并非真实的接口地址
+      header: {
+        'content-type': 'application/json', // 默认值
+        'Authorization': wx.getStorageSync('token') // token
+      },
+      success (res) {
+        // 处理任务列表数据
+        console.log(res.data)
+      },
+      fail(err){
+        Toast.fail("不能获取数据，错误信息: " + err)
+      }
+    })
   }
 })
