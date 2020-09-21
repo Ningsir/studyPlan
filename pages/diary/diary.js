8e158aef079f3726da59045c34bfc2d815c628bd
Xinger <u201717024@hust.edu.cn>
2020-09-21 08:32:57 +0800

    mod diary

--- pages/diary/diary.js
+++ pages/diary/diary.js
@@ -1,52 +1,147 @@
+import Toast from '../../miniprogram_npm/@vant/weapp/toast/toast';
+
+const util = require('../../utils/util.js');
+const api = require("../../config/api")
 // pages/diary/diary.js
-const util=require('../../utils/util.js');
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    id: '',
-    //type: '', //标识是新建日记还是编辑日记
-    diaryTitle:'',
-    diaryContent:'',
-    diaryDate:''
+    index: -1, //仅在编辑日记条件下使用，提供所编辑的日记在日记列表中的索引
+    type: '', //标识是新建日记还是编辑日记
+    diaryTitle: '',
+    diaryContent: '',
+    diaryDate: '',
+    id:''
+  },
+  getDate() {
+    var date = new Date()
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    var _time = year + "-" + month + "-" + day
+    return _time
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var obj =JSON.parse(options.obj)
-    console.log(obj)
     var type = options.type
-    if(type=='1'){ //表明是编辑日记
+    var index = options.id
+    //type:'1' && index != -1 -- 编辑日记
+    //type='2' && index = -1 -- 新建日记
+    this.setData({
+      type: type,
+      index: index
+    })
+    if (type == '1') { //表明是编辑日记
+      var obj = JSON.parse(options.obj)
       this.setData({
         diaryTitle: obj.title,
         diaryDate: obj.date,
-        diaryContent: obj.content
+        diaryContent: obj.content,
+        id: obj.id
       })
-    }else{
-      var diaryTime=util.formatTime(new Date())
+    } else { //表明是新建日记
+      var diaryTime = this.getDate()
       this.setData({
-        diaryTime:diaryTime,
+        diaryDate: diaryTime,
       })
+      
     }
   },
 
-  OnInputTitleChange(event){
+  OnInputTitleChange(event) {
     this.setData({
-        diaryTitle:event.detail
+      diaryTitle: event.detail
     })
-    console.log(this.data.diaryTitle)
+     
   },
 
-  OnInputContentChange(event){
+  OnInputContentChange(event) {
     this.setData({
-        diaryContent:event.detail
+      diaryContent: event.detail
     })
-    console.log(this.data.diaryContent)
+    
   },
-  confirmDairy(event){
-    //这里把修改后的日记提交到后端
-    //提交id,title,date,content
+  save(event) {
+    var newdiary = {
+      "date": this.data.diaryDate,
+      "title": this.data.diaryTitle,
+      "content": this.data.diaryContent
+    }
+    let pages = getCurrentPages(); //页面栈
+    let curPage = pages[pages.length - 2]; //上一页面（diaryList）
+
+    if (this.data.type == '1') { //保存编辑过的日记
+      //splice用新的替换原alldiary中的对应项
+      newdiary['id'] = this.data.id
+      this.updateDiaryById(newdiary)
+      curPage.data.alldiary.splice(this.data.index, 1, newdiary)
+    } else {
+      //TODO 把修改后的alldiary提交到后端
+      //需提交的数据：title,date,content
+      var date = this.data.diaryDate
+      var title = this.data.diaryTitle
+      var content = this.data.diaryContent
+      var _data = {
+        "title": title,
+        "content": content
+      }
+      this.createDiary(_data)
+      //跳转到日记列表，同时将插入新日记后的列表
+      curPage.data.alldiary.push(newdiary)
+    }
+    curPage.setData({
+      alldiary: curPage.data.alldiary
+    })
+    wx.navigateBack({
+      delta: 1,
+    })
+  },
+  updateDiaryById(data){
+    wx.request({
+      url: api.updateDiaryById, //仅为示例，并非真实的接口地址,
+      data: data,
+      method: "POST",
+      header: {
+        'content-type': 'application/json', // 默认值
+        'Authorization': wx.getStorageSync('token') // token
+      },
+      success(res) {
+        // 处理任务列表数据 
+        if (res.statusCode === 200) {       
+          Toast.success("修改成功")
+        } else {
+          Toast.fail("修改失败")
+        }
+      },
+      fail(err) {
+        Toast.fail("修改失败")
+      }
+    })
+  },
+  createDiary(data){
+    wx.request({
+      url: api.addDiary, //仅为示例，并非真实的接口地址,
+      data: data,
+      method: "POST",
+      header: {
+        'content-type': 'application/json', // 默认值
+        'Authorization': wx.getStorageSync('token') // token
+      },
+      success(res) {
+        // 处理任务列表数据 
+        if (res.statusCode === 200) {       
+          Toast.success("保存成功")
+        } else {
+          Toast.fail("保存失败")
+        }
+      },
+      fail(err) {
+        Toast.fail("保存失败")
+      }
+    })
   }
 })
--- pages/diaryList/diaryList.js
+++ pages/diaryList/diaryList.js
@@ -1,3 +1,7 @@
+import Toast from '../../miniprogram_npm/@vant/weapp/toast/toast';
+import Dialog from '../../miniprogram_npm/@vant/weapp/dialog/dialog';
+
+const api = require("../../config/api")
 // pages/diaryList/diaryList.js
 Page({
 
@@ -5,34 +9,138 @@ Page({
    * 页面的初始数据
    */
   data: {
-    alldiary: [
-      {
-        id:'',
-        date: "2020/9/16",
-        title: "今天是个好日子",
-        content:'四点九四大家多少就是\n很多时候倒i建瓯但是佛'
-      },
-      {
-        id:'',
-        date: "2020/9/15",
-        title: "哈哈哈哈哈",
-        content: '大家撒电视机分厘卡是\n觉得死哦附件是\n'
-      },
-    ],
-    slideButtons:[{
+    alldiary: [],
+    slideButtons: [{
       type: 'warn',
       text: '删除',
       src: '/page/weui/cell/icon_del.svg', // icon的路径
-    }]
+    }],
+    loading: true
   },
-  diaryPress(event){
-    var id = parseInt(event.currentTarget.id)
-    console.log(id)
-    var obj = JSON.stringify(this.data.alldiary[id]);
-    wx.navigateTo({
-      url: '/pages/diary/diary?type=1&obj=' + obj,
+  onLoad: function (options) { //只触发一次
+    this.setData({
+      loading: true
+    })
+    //TODO 从后端获取所有的日记记录，更新this.data.alldiary
+    this.getTotalDiary()
+    //对现有日记按日期先后排序
+    this.data.alldiary.sort(this.sortByProperty("date"))
+    this.setData({
+      alldiary: this.data.alldiary
     })
   },
-  slideButtonTap(event){
-  }
+  /**
+   * 获取日记列表
+   */
+  getTotalDiary: function(){
+    let that = this
+    wx.request({
+      url: api.getTotalDiary, //仅为示例，并非真实的接口地址
+      header: {
+        'content-type': 'application/json', // 默认值
+        'Authorization': wx.getStorageSync('token') // token
+      },
+      success(res) {
+        // 处理任务列表数据 
+        if (res.statusCode === 200) {
+          that.setData({
+            alldiary: res.data,
+            loading: false
+          })
+          console.log(that.data.alldiary)
+          Toast.success("加载成功")
+        } else {
+          Toast.fail("加载失败")
+        }
+      },
+      fail(err) {
+        that.setData({
+          loading: false
+        })
+        Toast.fail("无法获取日记数据")
+      }
+    })
+  },
+  /**
+   * 删除日记
+   */
+  deleteDiary(index) {
+    let that = this
+    let id = this.data.alldiary[index].id
+    Dialog.confirm({
+        title: '删除日记',
+        message: '确定要删除日记吗？',
+        zIndex: 999
+      })
+      .then(() => {
+        // 删除
+        wx.request({
+          url: api.deleteDiaryById + "?id=" + id,
+          header: {
+            'content-type': 'application/json', // 默认值
+            'Authorization': wx.getStorageSync('token')
+          },
+          success(res) {
+            if (res.statusCode === 200) {
+              that.onLoad()
+              Toast.success("删除日记成功")
+            }
+            if (res.statusCode != 200) {
+              Toast.fail("删除日记失败")
+            }
+          },
+          fail(err) {
+            Toast.fail("删除日记失败")
+          }
+        })
+      })
+      .catch(() => {
+        // on cancel
+      });
+  },
+  sortByProperty(property) {
+    function sortcore(obj1, obj2) {
+      if (obj1[property] > obj2[property]) return 1
+      else if (obj1[property] < obj2[property]) return -1
+      else if (obj1[property] == obj2[property]) return 0
+    }
+    return sortcore
+  },
+  onShow: function () { //刷新日记列表
+    // this.onLoad()
+  },
+  onPullDownRefresh: function () {
+    this.onLoad()
+  },
+  diaryPress(event) {
+    var id = parseInt(event.currentTarget.id)
+    var obj = JSON.stringify(this.data.alldiary[id]);
+    wx.navigateTo({
+      url: '/pages/diary/diary?id=' + id + '&type=1&obj=' + obj,
+    })
+  },
+  slideButtonTap(event) { //删除当前一条日记记录
+    var curId = event.currentTarget.id
+    this.deleteDiary(curId)
+    // var item = this.data.alldiary.splice(curId, 1)
+    // this.setData({
+    //   alldiary: this.data.alldiary
+    // })
+  },
+
+  // /**
+  //  * 返回
+  //  */
+  // onClickLeft(){
+  //   wx.navigateBack({
+  //     delta: 1,
+  //   })
+  // },
+
+  // /**
+  //  * 保存日记
+  //  */
+  // onClickRight(){
+
+  // }
 })
